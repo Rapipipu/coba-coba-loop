94e40b6a666e0220871f5844054143c1e6f969e7
Muhammad Rafi Satria <89404089+Rapipipu@users.noreply.github.com>
2021-11-24 15:06:32 +0700

    Add files via upload

---
+++ coba coba for loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int N; cin >> N;
+	int nilai[N];
+	
+	for(int i=0; i<N; i++){
+		cin >> nilai[i];
+		
+		
+	}for(int i=0; i<N; i++){
+		if(nilai[i]<15){
+			cout << "Tidak Lulus" << endl;
+		}else{
+			cout << "Lulus" << endl;
+		}	
+}
+}
